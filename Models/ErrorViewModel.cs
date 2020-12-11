f20a25770796e8f199c50dc34abf48ecbac021ed
Lavanya <lavanya1218.g@gmail.com>
2020-12-10 20:38:39 -0600

    app inializition

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hakucarrentalsMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
